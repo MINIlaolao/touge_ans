1a02359a917ec6d81ded57e5d979c447be591988
MINIlaolao <30876219+MINIlaolao@users.noreply.github.com>
2022-12-19 12:06:53 +0800

    Create ospf_mult.sh

---
+++ ospf_mult.sh
@@ -0,0 +1,26 @@
+hostname Router0
+--
+router ospf 1
+--
+ network 192.168.10.0 0.0.0.255 area 1
+ network 192.168.1.0 0.0.0.255 area 1
+ network 10.0.0.0 0.0.0.255 area 1
+hostname Router1
+--
+router ospf 1
+--
+ network 192.168.1.0 0.0.0.255 area 1
+ network 192.168.2.0 0.0.0.255 area 0
+hostname Router2
+--
+router ospf 1
+--
+ network 192.168.2.0 0.0.0.255 area 0
+ network 192.168.3.0 0.0.0.255 area 2
+hostname Router3
+--
+router ospf 1
+--
+ network 192.168.3.0 0.0.0.255 area 2
+ network 192.168.20.0 0.0.0.255 area 2
+ network 40.0.0.0 0.0.0.255 area 2
